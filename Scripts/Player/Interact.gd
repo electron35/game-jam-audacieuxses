45f83b93720be4411f58d1bff0cd81aa47778313
Malotrue <115473836+Malotrue@users.noreply.github.com>
2025-09-28 15:58:43 +0200

    Inté son wand

--- Scripts/Player/Interact.gd
+++ Scripts/Player/Interact.gd
@@ -1,10 +1,13 @@
 extends RayCast3D
 @onready var wand: AnimatedSprite2D = $"../UIControl/Wand"
+@onready var audio_stream_player: AudioStreamPlayer = $AudioStreamPlayer
 
 
 func _input(event):
 	if event.is_action_pressed("Shoot"):
 		wand.play("default")
+		audio_stream_player.play()
+		
 		var item:Node3D
 		if (get_collider()!=null):
 			item = get_collider()
