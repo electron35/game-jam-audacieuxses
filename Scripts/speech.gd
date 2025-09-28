4512b3ac2467dbf7f4e9628ef86745e33540969f
Iris Eleque <eleque.turone@outlook.fr>
2025-09-28 14:29:17 +0200

    Update to environment and speech bubble

--- Scripts/speech.gd
+++ Scripts/speech.gd
@@ -3,13 +3,10 @@ extends Sprite3D
 
 @export var display_text:String = "default"
 @onready var vp: SubViewport = $SubViewport
-@onready var box: NinePatchRect = $SubViewport/Box
-@onready var label: Label = $SubViewport/Box/Label
+@onready var label: Label = $SubViewport/Label
 
 
 func _process(delta: float) -> void:
 	if (label!=null):
 		label.text = display_text
-	if (box!= null && label!=null):
-		box.size = label.size
-		vp.size = box.size
+		#vp.size = label.size
