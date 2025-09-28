5a2ede876ba888fef3d70dfee1998e26e402b77c
Malotrue <115473836+Malotrue@users.noreply.github.com>
2025-09-28 17:03:57 +0200

    Inté son projectile_impact

--- Scripts/Player/Interact.gd
+++ Scripts/Player/Interact.gd
@@ -1,16 +1,18 @@
 extends RayCast3D
 @onready var wand: AnimatedSprite2D = $"../UIControl/Wand"
-@onready var audio_stream_player: AudioStreamPlayer = $AudioStreamPlayer
+@onready var projectile_throw: AudioStreamPlayer = $projectile_throw
+@onready var projectile_impact: AudioStreamPlayer = $projectile_impact
 
 
 func _input(event):
 	if event.is_action_pressed("Shoot"):
 		wand.play("default")
-		audio_stream_player.play()
+		projectile_throw.play()
 		
 		var item:Node3D
 		if (get_collider()!=null):
 			item = get_collider()
 			item.spell()
+			projectile_impact.play()
 		
 			
