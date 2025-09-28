7e79596181b22f65b843353ac1b7264b823c0e67
Iris Eleque <eleque.turone@outlook.fr>
2025-09-28 16:29:56 +0200

    Victory

--- Scripts/Player/UI_Scoring.gd
+++ Scripts/Player/UI_Scoring.gd
@@ -3,9 +3,9 @@ extends Sprite2D
 @onready var progress_bar: TextureProgressBar = $ProgressBar
 
 func _process(delta: float) -> void:
-	var max:float = Global.MaxScore
-	var cur:float = Global.Score
-	if (max!=0):
-		progress_bar.value = (Global.Score/max)*100
+	var maxScore:float = Global.MaxScore
+	var curScore:float = Global.Score
+	if (maxScore!=0):
+		progress_bar.value = (curScore/maxScore)*100
 	else:
 		progress_bar.value = 0
--- Scripts/score.gd
+++ Scripts/score.gd
@@ -1,10 +1,24 @@
 extends Control
 
-@onready var label: Label = $Label
+@onready var ui_winning: Sprite2D = $UiWinning
+@onready var ui_game_over: Sprite2D = $UiGameOver
+@onready var option_good_end_3: Sprite2D = $OptionGoodEnd3
+
 
 func _ready() -> void:
 	var text = "Incroyable ton score est de " + str(Global.Score) 
-	label.text = text
+	var maxScore:float = Global.MaxScore if Global.MaxScore!=0 else 1
+	var curScore:float = Global.Score
+	
+	if curScore/maxScore*100 < 30:
+		ui_winning.visible = false
+		option_good_end_3.visible = false
+		ui_game_over.visible = true
+	else:
+		ui_game_over.visible = false
+		option_good_end_3.visible = true
+		ui_winning.visible = true
+	
 
 
 func _on_button_pressed() -> void:
