5a13c7b4c29c08a7b60e5b84996d87ab9cc36706
Malotrue <115473836+Malotrue@users.noreply.github.com>
2025-09-28 15:09:12 +0200

    Inté son chrono burning

--- Scripts/timer_clock.gd
+++ Scripts/timer_clock.gd
@@ -3,14 +3,15 @@ extends Control
 @onready var chrono: Sprite2D = $Chrono
 @onready var chrono_arrow: Sprite2D = $ChronoArrow
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
-@onready var timer: Timer = $Timer
-
+@onready var timer: Timer = $Timer 
+@onready var chrono_bruning: AudioStreamPlayer = $chrono_bruning 
 
 var wait_time:float
 
 func _ready() -> void:
 	wait_time = timer.wait_time
 	timer.start()
+	chrono_bruning.play()
 
 func _process(delta: float) -> void:
 	chrono_arrow.rotate(deg_to_rad(360/wait_time)*delta)
